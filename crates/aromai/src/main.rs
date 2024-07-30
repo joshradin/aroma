295933e9955fa27c6de05bde161e285cf93db1a1
joshradin <jradin16@gmail.com>
2024-07-29 19:13:35 -0700

    adding logger

--- crates/aromai/src/main.rs
+++ crates/aromai/src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stderr, stdout};
+use std::process::ExitCode;
+use chrono::{DateTime, Utc};
+use clap::Parser;
+use log::{Level, LevelFilter, trace};
+use owo_colors::OwoColorize;
+use owo_colors::Stream::Stdout;
+
+fn main() -> eyre::Result<ExitCode> {
+    color_eyre::install()?;
+    let app = App::parse();
+    init_logging(app.log.unwrap_or(LevelFilter::Info))?;
+    trace!("starting aromi with args {app:#?}");
+
+    Ok(ExitCode::SUCCESS)
+}
+
+
+fn init_logging(level_filter: LevelFilter) -> eyre::Result<()> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{} {} {} --- [] : {}",
+                Utc::now().format("%Y-%m-%d %H:%M:%S%.3f"),
+                record.level()
+                    .if_supports_color(Stdout, |text| match text {
+                        Level::Error => { text.bright_red().to_string()}
+                        Level::Warn => { text.bright_yellow().to_string()}
+                        Level::Info => { text.green().to_string()}
+                        Level::Debug => { text.blue().to_string()}
+                        Level::Trace => { text.purple().to_string()}
+                    }),
+                record.target(),
+                message
+            ))
+        })
+        .level(level_filter)
+        .chain(stdout())
+        .apply()?;
+    Ok(())
+}
+
+#[derive(Debug, Parser)]
+struct App {
+    #[clap(long="log-level", env="RUST_LOG")]
+    log: Option<LevelFilter>
 }
